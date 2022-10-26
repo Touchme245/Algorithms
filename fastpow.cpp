697b8fe89f0f5406ea74f2fc57117e63c772e42b
Touchme245 <nikitanovikov245@yandex.ru>
2022-10-27 01:18:20 +0300

    Быстрое возведение в степень за logn

---
+++ fastpow.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+
+
+long long bpow(long long x, long long y){
+    int res = 1;
+    while(y > 0) {
+        if (y & 1){
+            res = (res * x) % MOD;
+        }
+        x = (x * x) % MOD;
+        y = y >> 1;
+    }
+
+    return res;
+}
+
+
+int main() {
+    long long a,b;
+    cin >> a >> b;
+    cout << bpow(a,b) << endl;
+
+}
